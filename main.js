faf921f4c6383c5040f520afdfaf21e0f9dc08b5
yitanyalyDev <yitanyaly.code@gmail.com>
2025-04-05 16:25:58 +0200

    [webflow] script change test_2

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const whatsappButtons = document.querySelectorAll('.whatsapp-button');
-    const productId = document.getElementById('name-product'); 
+    // const whatsappButtons = document.querySelectorAll('.whatsapp-button');
+    // const productName = document.getElementById('name-product'); 
+    // print(productName);
+
+    // whatsappButtons.forEach(button => {
+    //   button.addEventListener('click', function () {
+    //     const phoneNumber = '573112135011'; 
+    //     const message = `Buen dia, estoy interesado en el producto: ${productName}`;
+    //     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    //     window.open(url, '_blank');
+    //   });
+    // });
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const submitButton = document.getElementById('whatsapp-button'); 
+        const productName = document.getElementById('name-product'); // Este es el elemento que contiene el nombre del producto
+        console.warn(productName);
+        print(productName);
+        console.log(productName, 'entro el producto')
     
-    whatsappButtons.forEach(button => {
-      button.addEventListener('click', function () {
-        const phoneNumber = '573112135011'; 
-        const message = `Buen dia, estoy interesado en el producto: ${productId}`;
-        const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-        window.open(url, '_blank');
+        if (submitButton && productName) {
+          submitButton.addEventListener('click', function () {
+            const phoneNumber = '573112135011'; 
+            const product = productName.textContent.trim(); 
+            const message = `Buen día, estoy interesado en el producto: ${product}`;
+            const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+            window.open(url, '_blank');
+          });
+        } else {
+          console.warn("Botón o nombre del producto no encontrados.");
+        }
       });
-    });
-  });
