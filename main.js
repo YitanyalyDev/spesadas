c7514876752f38b76483b569415b8ee081c12fbc
Yitanyaly Velandia <83384423+YitanyalyDev@users.noreply.github.com>
2025-04-26 11:29:28 +0200

    [release] add url in the message

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
     document.addEventListener('DOMContentLoaded', function () {
         const submitButton = document.getElementById('whatsapp-button'); 
-        const productName = document.getElementById('name-product'); // Este es el elemento que contiene el nombre del producto
+        const productName = document.getElementById('name-product'); 
         console.warn(productName);
         console.log(productName, 'entro el producto')
     
@@ -8,7 +8,8 @@
           submitButton.addEventListener('click', function () {
             const phoneNumber = '573128522568'; 
             const product = productName.textContent.trim(); 
-            const message = `Buen día, estoy interesado en el producto: ${product}`;
+            const currentUrl = window.location.href;
+            const message = `Buen día, estoy interesado en el producto: ${product} que encontre en la página web: ${currentUrl}`;
             const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
             window.open(url, '_blank');
           });
