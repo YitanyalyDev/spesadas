d63350af31537bf941ae8e97b3fe7fad9a56c690
Yitanyaly Velandia <83384423+YitanyalyDev@users.noreply.github.com>
2025-04-26 17:17:49 +0200

    Change but page start

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-    document.addEventListener('DOMContentLoaded', function () {
-        const submitButton = document.getElementById('whatsapp-button'); 
-        const productName = document.getElementById('name-product'); 
-        console.warn(productName);
-        console.log(productName, 'entro el producto')
-    
-        if (submitButton && productName) {
-          submitButton.addEventListener('click', function () {
-            const phoneNumber = '573128522568'; 
+document.addEventListener('DOMContentLoaded', function () {
+    const submitButton = document.getElementById('whatsapp-button'); 
+    const productName = document.getElementById('name-product'); // Nombre del producto
+    const linkProduct = document.getElementById('link-product'); // Link personalizado del producto
+
+    if (submitButton && productName) {
+        submitButton.addEventListener('click', function () {
+            const phoneNumber = '573112135011'; 
             const product = productName.textContent.trim(); 
-            const currentUrl = window.location.href;
-            const message = `Buen día, estoy interesado en el producto: ${product} que encontre en la página web: ${currentUrl}`;
+            
+            let currentUrl = window.location.href; // Valor por defecto
+            if (linkProduct && linkProduct.textContent.trim() !== '') {
+                currentUrl = linkProduct.textContent.trim(); // Usamos el link-product si existe y no está vacío
+            }
+
+            const message = `Buen día, estoy interesado en el producto: ${product}. Aquí vi el producto: ${currentUrl}`;
             const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
             window.open(url, '_blank');
-          });
-        } else {
-          console.warn("Botón o nombre del producto no encontrados.");
-        }
-      });
+        });
+    } else {
+        console.warn("Botón o nombre del producto no encontrados.");
+    }
+});
